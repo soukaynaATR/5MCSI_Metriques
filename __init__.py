4e40e4663b3f22048ea1bda4dae547f18b319314
soukaynaATR <124044212+soukaynaATR@users.noreply.github.com>
2026-01-08 12:07:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,6 +7,10 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)   
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
